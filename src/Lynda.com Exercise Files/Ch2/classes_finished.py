64f0cea88d3464a3dde9b29f7d507ef359d51096
Vikram D'Mello <vix@Prollo-2.local>
2020-06-22 16:55:48 -0400

    Wrapping up Birthdays.ipynb

--- src/Lynda.com Exercise Files/Ch2/classes_finished.py
+++ src/Lynda.com Exercise Files/Ch2/classes_finished.py
@@ -14,13 +14,13 @@ class anotherClass(myClass):
     print ("anotherClass method2")
     
   def method1(self):
-    myClass.method1(self);
+    myClass.method1(self)
     print ("anotherClass method1")
       
 def main():
-  c = myClass()
-  c.method1()
-  c.method2("This is a string")
+  #c = myClass()
+  myClass().method1()
+  myClass().method2("This is a string")
   c2 = anotherClass()
   c2.method1()
   
